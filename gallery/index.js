1c9de4e0ed1883fb56e6900741e31cfe4b8beb6c
Mari <96116756+fox1206@users.noreply.github.com>
2022-02-14 18:29:27 +0300

    feat: add project gallery
    api

---
+++ gallery/index.js
@@ -0,0 +1,78 @@
+// 3WpPCmsMiCip2kElqstSmR-fsccqRSrk7I9biUSX5R0
+// vpumRqQM11x9Ksij9vSDdijBDtI-MxvB7zK59SAcDWs
+let url = 'https://api.unsplash.com/search/photos?query=girl&per_page=30&orientation=landscape&client_id=3WpPCmsMiCip2kElqstSmR-fsccqRSrk7I9biUSX5R0';
+
+const main = document.querySelector('.main');
+const gallery = document.querySelector('.container_main');
+const searchWord = document.querySelector('.input_search');
+const button = document.querySelector('.search_img');
+const view = document.querySelector('.view');
+const closeImg = document.querySelector('.close');
+
+//получаю данные с сервера
+async function getData() {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  showData(data);
+}
+
+//поиск и обновление
+function inputClick(){
+  // console.log(`${searchWord.value}`);
+  url = `https://api.unsplash.com/search/photos?query=${searchWord.value}&per_page=15&orientation=landscape&client_id=3WpPCmsMiCip2kElqstSmR-fsccqRSrk7I9biUSX5R0`;
+  // console.log(url);
+  // getData(url);
+  gallery.innerHTML = '';
+  getData(url);
+}
+
+// заполнение фотографиями
+function showData(data) {
+  data.results.map((el) => {
+    const picture = document.createElement('div');
+    picture.classList.add('picture');
+    picture.style.backgroundImage = `url('${el.urls.regular}')`;
+    gallery.append(picture);
+
+    picture.addEventListener('mouseover', () => {
+      picture.classList.toggle('active');
+    });
+
+    picture.addEventListener('click', () => {
+        main.addEventListener('click', () => {
+          view.style.backgroundImage = `url('${el.urls.regular}')`;
+          view.classList.toggle('view_gal');
+      });
+    });
+  });
+}
+
+//события
+getData();
+button.addEventListener('click', inputClick);
+
+const audio = document.querySelector('.audio');
+const api = document.querySelector('.api_color');
+
+// музыка на старте
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function startSound() {
+  if (audio.paused) {
+    audio.play();
+  }
+  else {
+    audio.pause();
+  }
+}
+
+playAudio();
+
+api.addEventListener('click', startSound);
